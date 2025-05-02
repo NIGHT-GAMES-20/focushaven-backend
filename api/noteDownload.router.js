5231c43055ed2ac02e520d68d0274c362f440b5a
Abhinash Dey <night20games@gmail.com>
2025-05-02 19:45:38 +0530

    note Downloader Added

---
+++ api/noteDownload.router.js
@@ -0,0 +1,38 @@
+import { Router } from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const router = Router()
+
+export default async function noteDownload(client, drive) {
+    router.get('/:fileName', async (req, res) => {
+        const fileName = req.params.fileName
+        
+        const db = client.db('focushaven')
+        const notesCollection = db.collection('notes')
+
+
+        const file = await notesCollection.findOne({ topic: fileName });
+        const fileId = file.url; // Assuming the file ID is stored in the 'url' field
+
+        try {
+            const driveResponse = await drive.files.get(
+                { fileId, alt: 'media' },
+                { responseType: 'stream' }
+            )
+        
+            res.setHeader('Content-Disposition', `attachment; filename="${fileName}.${file.extension}"`)
+            res.setHeader('Content-Type', 'application/octet-stream')
+        
+            driveResponse.data
+                .on('end', () => res.status(200).send('File downloaded successfully'))
+                .on('error', (err) => {
+                res.status(500).send(`Error downloading file.: ${err}`)
+                })
+                .pipe(res)
+        } catch (err) {
+        res.status(500).json({ message: 'Failed to download file', error: err.message })
+        }
+    })
+    return router;
+}
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import login from "./api/login.router.js"
 import signin from "./api/signin.router.js"
 import notes from "./api/notes.router.js";
 import notesUpdate from "./api/noteUpdate.router.js";
+import noteDownload from "./api/noteDownload.router.js";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 
@@ -23,6 +24,7 @@ export default async function CreateServer(client,drive){
     app.use("/api/v1/login", await login(client))
     app.use("/api/v1/notes", notes(client,drive))
     app.use("/api/v1/notes/update", notesUpdate(client,drive))
+    app.use("/api/v1/downloads/notes/download", await noteDownload(client,drive))
     app.use("/api/v1/signin", signin)
     app.use("/api/v1/uptime-keeper", (req, res) =>  res.status(200).json({ message: "Uptime keeper is working!" }));
     app.use("/api/v1/test", (req, res) => res.status(402).json({"Test":"Testing"}));
