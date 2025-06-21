e4dbcb1328abcb03edb197f491de2e0f99fb24c6
Abhinash Dey <night20games@gmail.com>
2025-06-21 15:58:06 +0530

    OTP CLeanUp Script Optimized

--- scripts/OTPCleanUp.js
+++ scripts/OTPCleanUp.js
@@ -3,13 +3,13 @@ import cron from 'node-cron';
 export default function OTPCleanup(client) {
   const collection = client.collection("otps");
 
-  // Run daily at 3:00 AM (server time)
   cron.schedule('0 * * * *', async () => {
     try {
       const now = Date.now();
-      const ISOTimestamp = new Date().toISOString();
       const result = await collection.deleteMany({ DeleteAt: { $lt: now } });
-      console.log(`[CRON] Deleted ${result.deletedCount} expired OTPs at ${ISOTimestamp}`);
+      if(result.deletedCount !== 0) {
+        console.log(`[CRON] Deleted ${result.deletedCount} expired OTPs at ${new Date().toISOString()}`);
+      }
     } catch (error) {
       console.error("[CRON] OTP cleanup failed:", error.message);
     }
