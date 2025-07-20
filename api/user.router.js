6cde8bd64f28f2e79d3b16e5aef5b2279f6a916f
Abhinash Dey <night20games@gmail.com>
2025-07-20 08:59:44 +0530

    userInfo Routers Added

---
+++ api/user.router.js
@@ -0,0 +1,32 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+export default async function userRoute(client){
+  const router = express.Router();
+
+  router.get("/getUserInfo", (req, res) => {
+    const token = req.cookies.authToken;
+    if (!token) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    if (!decoded) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+    const userCollection = client.collection("username_passwords");
+    userCollection.findOne({ username: decoded.username }, { projection: { password: 0 } })
+      .then(user => {
+        if (!user) {
+          return res.status(404).json({ error: "User not found" });
+        }
+        res.status(200).json({success: true, user });
+      })
+      .catch(err => {
+        console.error("Error fetching user info:", err);
+        res.status(500).json({ error: "Internal server error" });
+      });    
+  });
+
+}
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import notes from "./api/notes.router.js";
 import notesUpdate from "./api/noteUpdate.router.js";
 import noteDownload from "./api/noteDownload.router.js";
 import forum from "./api/forum.router.js";
+import userRoute from "./api/user.router.js";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 
@@ -28,6 +29,7 @@ export default async function CreateServer(client,drive) {
     app.use("/api/v1/downloads/notes/download", await noteDownload(client,drive))
     app.use("/api/v1/forum", await forum(client))
     app.use("/api/v1/signin", signin(client))
+    app.use("/api/v1/user", userRoute(client));
     app.use("/api/v1/uptime-keeper", (req, res) =>  res.status(200).json({ message: "Uptime keeper is working!" }));
     app.use("/api/v1/test", (req, res) => res.status(402).json({"Test":"Testing"}));
     app.use("*name", (req, res) => res.status(404).json({"error":"Not Found"}));
